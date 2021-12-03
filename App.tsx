6f40fc188da614977dcf2bb4b2f0a498642ca88b
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-12-03 17:50:05 -0300

    feat: project styles

--- App.tsx
+++ App.tsx
@@ -1,11 +1,42 @@
 
+import AppLoading from 'expo-app-loading';
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import {
+  useFonts,
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold
+} from '@expo-google-fonts/archivo';
+import {
+  Inter_400Regular,
+  Inter_500Medium
+} from '@expo-google-fonts/inter';
+
 import { Home }  from './src/screens/Home';
+import theme from './src/styles/theme';
 
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
-    <Home />
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+
   );
 }
 
