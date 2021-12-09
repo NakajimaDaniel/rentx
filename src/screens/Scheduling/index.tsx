13639eeb723d0269ac9122aaf16be4398ad32f0b
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-12-09 19:28:54 -0300

    feat: schedule screen

---
+++ src/screens/Scheduling/index.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { useTheme } from 'styled-components';
+import { BackButton } from '../../components/BackButton';
+
+import ArrowSvg from '../../assets/arrow.svg'
+
+import {
+  Container,
+  Header,
+  Title,
+  RentalPeriod,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  Content,
+  Footer,
+} from './styles';
+import { StatusBar } from 'react-native';
+import { Button } from '../../components/Button';
+
+export function Scheduling() {
+
+  const theme = useTheme();
+
+  return (
+    <Container>
+
+      <StatusBar 
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      
+      />
+
+      <Header>
+        <BackButton 
+          color={theme.colors.shape}
+          onPress={() =>{}}
+        />
+        <Title>
+          Escolha uma {'\n'}
+          data de inicio e {'\n'}
+          fim de aluguel
+        </Title>
+
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue selected={false} ></DateValue>
+          </DateInfo>
+          <ArrowSvg />
+          <DateInfo>
+            <DateTitle>ATE</DateTitle>
+            <DateValue  selected={false} ></DateValue>
+          </DateInfo>
+        </RentalPeriod>
+
+      </Header>
+  
+      <Content>
+
+      </Content>
+
+      <Footer>
+        <Button 
+          title="confirmar"
+        />
+      </Footer>
+
+    </Container>
+  )
+}
