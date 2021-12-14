7eea89fca3c44f1320554d93ef7eee264ba3a9c2
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-12-14 19:27:44 -0300

    feat: retrieve data using json-server

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,11 @@
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import Logo from '../../assets/logo.svg'
 import { Car } from '../../components/Car';
+
+import api from '../../services/api';
 
 import {
   Container,
@@ -31,6 +33,15 @@ export function Home() {
   function handleCarDetails () {
     navigation.navigate("CarDetails");
   }
+
+  useEffect(() => {
+    async function fetchCars() {
+      const response = await api.get('/cars');
+      console.log(response);
+    }
+    fetchCars();
+
+  }, [])
 
   return (
     <Container>
