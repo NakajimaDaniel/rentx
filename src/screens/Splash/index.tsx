110af83dde01404a46514166bc0016df7dd119d7
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-12-31 19:04:12 -0300

    feat: using worklet

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -2,16 +2,18 @@ import React, { useEffect } from 'react';
 import BrandSvg from '../../assets/brand.svg'
 import LogoSvg from '../../assets/logo.svg'
 
-import Animated, { useSharedValue, useAnimatedStyle, withTiming, Easing, interpolate } from 'react-native-reanimated';
+import Animated, { useSharedValue, useAnimatedStyle, withTiming, Easing, interpolate, runOnJS } from 'react-native-reanimated';
 
 
 import {
   Container,
 } from './styles';
+import { useNavigation } from '@react-navigation/native';
 
 export function Splash() {
 
   const splashAnimation = useSharedValue(0);
+  const navigation = useNavigation();
 
   const brandStyle = useAnimatedStyle(() => {
     return {
@@ -24,7 +26,6 @@ export function Splash() {
     }
   })
 
-
   const logoStyle = useAnimatedStyle(() => {
     return {
       opacity: interpolate(splashAnimation.value, [0, 25, 50], [0, .3, 1]),
@@ -36,8 +37,16 @@ export function Splash() {
     }
   })
 
+  function startApp() {
+    navigation.navigate('Home');
+  }
+
   useEffect(() => {
-    splashAnimation.value = withTiming(50, { duration: 1000 });
+    splashAnimation.value = withTiming(50, { duration: 1000 }, () => { 
+      'worklet'
+      runOnJS(startApp)();
+      
+    });
   }, [])
 
   return (
