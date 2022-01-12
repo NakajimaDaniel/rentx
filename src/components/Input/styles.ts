3f6010997359cda306faa71d6b68daeb59e8daa7
NakajimaDaniel <nakajimadaniel96@gmail.com>
2022-01-12 13:41:49 -0300

    fix: margin bottom to input

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -4,8 +4,8 @@ import styled from 'styled-components/native'
 
 export const Container = styled.View `
   width: 100%;
-  flex-direction: row;
-
+  flex-direction : row;
+  margin-bottom: 8px;
 `
 export const IconContainer = styled.View `
   height: 56px;
